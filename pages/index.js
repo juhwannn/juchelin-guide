a319708fcfc2e7dcc559fa189de9d16a81f62771
ahnjh <algorithm123@naver.com>
2023-01-21 19:37:27 +0900

    fix: ts -> js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export default function Home() {
+
+    return (
+        <div>
+            JUCHELIN-GUIDE <br/>
+
+            개인적인 맛집 리스트 입니다. <br/>
+
+
+        </div>
+    )
+}
